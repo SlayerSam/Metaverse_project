29d42955368abf132935f0bcdc7b453a822a304f
OmkarAnbhule <omkaranbhule75@gmail.com>
2024-08-02 00:25:29 +0530

    update scene

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Home from "@/components/home";
 
-export default function Home() {
+export default function App() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <Home />
     </main>
   );
 }
